264eb1be5735d715d874c91108a74e14943bdce1
aibekov_nurkerim <aibekovnurkerim@gmail.com>
2024-07-21 23:00:43 +0600

    Создал forms и urls для приложение

--- shop/store/models.py
+++ shop/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.name
--- shop/store/views.py
+++ shop/store/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Category
+from .forms import CategoryForm, ProductForm
 
-# Create your views here.
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+def product_view(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, 'product.html', {'product': product})
+
+def category_add_view(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories_view')
+    else:
+        form = CategoryForm()
+    return render(request, 'category_add.html', {'form': form})
+
+def product_add_view(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_view', id=product.id)
+    else:
+        form = ProductForm()
+    return render(request, 'product_add.html', {'form': form})
